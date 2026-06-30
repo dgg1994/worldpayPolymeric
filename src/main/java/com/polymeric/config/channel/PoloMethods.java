438575bca26080798636f8fd0f8d3446f3ee078e
cszadmin <15621349306@163.com>
2026-06-30 13:56:20 +0800

    默认polo上游基础配置，功能配置参数

---
+++ src/main/java/com/polymeric/config/channel/PoloMethods.java
@@ -0,0 +1,10 @@
+package com.polymeric.config.channel;
+
+public interface PoloMethods {
+
+    // 用户注册
+    public static String USER_REGISTER = "/user/register";
+
+    
+
+}
---
+++ src/main/java/com/polymeric/config/channel/UnifiedConfig.java
@@ -0,0 +1,16 @@
+package com.polymeric.config.channel;
+
+import lombok.Data;
+
+@Data
+public class UnifiedConfig {
+	
+	private String aprUrl;
+	
+	private String appId;
+	
+	private String rsaPrivateKey;
+	
+	private String aesKey;
+
+}
